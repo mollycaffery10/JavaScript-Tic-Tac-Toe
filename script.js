b640c8e7b3768f8e43a58e9d6563840deafa9a6b
jjblahut <julia.blahut@gmail.com>
2021-11-18 09:52:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const A_CLASS = 'X'
-const B_CLASS = 'O'
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -25,8 +25,8 @@ restartButton.addEventListener('click', startGame)
 function startGame() {
   circleTurn = false
   cellElements.forEach(cell => {
-    cell.classList.remove(A_CLASS)
-    cell.classList.remove(B_CLASS)
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
     cell.removeEventListener('click', handleClick)
     cell.addEventListener('click', handleClick, { once: true })
   })
@@ -36,7 +36,7 @@ function startGame() {
 
 function handleClick(e) {
   const cell = e.target
-  const currentClass = circleTurn ? B_CLASS : A_CLASS
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
   placeMark(cell, currentClass)
   if (checkWin(currentClass)) {
     endGame(false)
@@ -59,7 +59,7 @@ function endGame(draw) {
 
 function isDraw() {
   return [...cellElements].every(cell => {
-    return cell.classList.contains(A_CLASS) || cell.classList.contains(B_CLASS)
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
   })
 }
 
@@ -72,12 +72,12 @@ function swapTurns() {
 }
 
 function setBoardHoverClass() {
-  board.classList.remove(A_CLASS)
-  board.classList.remove(B_CLASS)
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
   if (circleTurn) {
-    board.classList.add(B_CLASS)
+    board.classList.add(CIRCLE_CLASS)
   } else {
-    board.classList.add(A_CLASS)
+    board.classList.add(X_CLASS)
   }
 }
 
